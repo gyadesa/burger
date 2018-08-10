a06a7c030def237d4fc89f0288ee3cf01cb9bead
Gamme Yadesa <gyadesa@gmail.com>
2018-08-10 12:51:10 -0500

    commiting directories and files created

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Gammes Super Burger');
+INSERT INTO burgers (name) VALUES ('Mammes Medum Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Solans Junior1 Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Bonas Junior1 Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Yonas Junior1 Burger', true);
+INSERT INTO burgers (name) VALUES ('Super slick Burger');
